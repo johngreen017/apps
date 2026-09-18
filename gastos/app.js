4442e68cab8dc8629f95dd1539c612993eb518da
johngreen017 <116195453+johngreen017@users.noreply.github.com>
2026-09-18 19:12:01 -0300

    Actualizar backend de gastos a nueva implementación

--- gastos/app.js
+++ gastos/app.js
@@ -1,4 +1,4 @@
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxK6qdkXtvKn17p8poyPkvR38ORhTQzuxcZV5H-xH2VevxMaxPR1IJ4TKMAG6sK3oSL4A/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbz2bVldJdW9vPuUCLuicFOvYhZueD8_gnC5EVZz3TOAfD4x8Po8CWX4J1aYZFpKNGAsOw/exec";
 
 const $ = (id) => document.getElementById(id);
 const money = new Intl.NumberFormat("es-CL",{style:"currency",currency:"CLP",maximumFractionDigits:0});
